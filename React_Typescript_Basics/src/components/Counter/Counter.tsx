968adfddf6abbe1a8792b4eaf8c857be6b6caa1f
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2025-03-10 21:24:42 +0530

    Counter

--- React_Typescript_Basics/src/components/Counter/Counter.tsx
+++ React_Typescript_Basics/src/components/Counter/Counter.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Counter.css";
 
-const Counter: React.FC = (props) => {
+const Counter: React.FC = () => {
+  const [CountVal, setCountVal] = useState<number>(0);
+
+  // const handleIncrement = () => {
+  //   setCountVal(CountVal + 1);
+  // };
+
+  // const handleDecrement = () => {
+  //   setCountVal(CountVal - 1);
+  // };
+
   return (
     <div className="counterContainer">
-      <h1>0</h1>
-      <button>INC</button>
-      <button>DEC</button>
+      <h1>{CountVal}</h1>
+      <button
+        onClick={() => setCountVal(CountVal + 1)}
+        style={{ backgroundColor: "lightgreen" }}
+      >
+        INC
+      </button>
+      <button
+        onClick={() => setCountVal(CountVal - 1)}
+        style={{ backgroundColor: "tomato" }}
+      >
+        DEC
+      </button>
     </div>
   );
 };
